382f3c2e8e433c7d2ae7b7e058f669312d2584ee
Lucky <hilaryomondi08@gmail.com>
2026-07-24 15:33:23 +0300

    finished the 6 table models

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from .database import Base
-from sqlalchemy import String,ForeignKey
+from sqlalchemy import String,ForeignKey,Numeric
 from sqlalchemy.orm import Mapped,mapped_column
+from decimal import Decimal
 
 class Users(Base):
     __tablename__="users"
@@ -20,3 +21,25 @@ class UserGroup(Base):
     user_group_id:Mapped[int]=mapped_column(primary_key=True)
     user_id:Mapped[int]=mapped_column(ForeignKey("users.user_id"))
     group_id:Mapped[int]=mapped_column(ForeignKey("groups.group_id"))
+
+class Expenses(Base):
+    __tablename__="expenses"
+
+    expense_id:Mapped[int]=mapped_column(primary_key=True)
+    expense_name:Mapped[str]=mapped_column(String(100))
+    amount:Mapped[Decimal]=mapped_column(Numeric(10,2))
+    group_id:Mapped[int]=mapped_column(ForeignKey("groups.group_id"))
+
+class Debt(Base):
+    __tablename__="debt"
+    user_id:Mapped[int]=mapped_column(ForeignKey("users.user_id"))
+    amount_owed:Mapped[Decimal]=mapped_column(Numeric(10,2))
+    debt_id:Mapped[int]=mapped_column(primary_key=True)
+    expense_id:Mapped[int]=mapped_column(ForeignKey("expenses.expense_id"))
+
+class Paid(Base):
+    __tablename__="paid"
+    user_id:Mapped[int]=mapped_column(ForeignKey("users.user_id"))
+    paid_amount:Mapped[Decimal]=mapped_column(Numeric(10,2))
+    paid_id:Mapped[int]=mapped_column(primary_key=True)
+    expense_id:Mapped[int]=mapped_column(ForeignKey("expenses.expense_id"))
