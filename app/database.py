122847c0cfe426b72294294bc7434791b8cf8163
Lucky <hilaryomondi08@gmail.com>
2026-07-31 11:04:50 +0300

    added the db function to database.py

--- app/database.py
+++ app/database.py
@@ -12,3 +12,10 @@ SessionLocal=sessionmaker(bind=engine)
 
 class Base(DeclarativeBase):
     pass
+
+def get_db():
+    db=SessionLocal()
+    try:
+       yield db
+    finally:
+        db.close()
