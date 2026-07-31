cadd12dd42944349f6b87e6c8f3e74ec9462ec0f
Lucky <hilaryomondi08@gmail.com>
2026-07-31 17:56:27 +0300

    wrote an instace for saving a user into the db

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI
-from .database import Base, engine
-from . import models 
+from fastapi import FastAPI, Depends
+from .database import Base, engine, get_db
+from .models import Users 
+from sqlalchemy.orm import Session
+from .schemas import UserCreate
 
 app=FastAPI()
 Base.metadata.create_all(bind=engine)
@@ -8,3 +10,19 @@ Base.metadata.create_all(bind=engine)
 @app.get("/")
 def read_root():
     return{"message":"Hello"}
+
+@app.post("/users")
+def user(user:UserCreate, db:Session=Depends(get_db)):
+    new_user=Users(user_name=user.user_name)
+
+    db.add(new_user)
+
+    db.commit()
+
+    db.refresh(new_user)
+
+    return new_user
+
+
+
+    
