4e98326c73ea6e6144830d5e8af0611e4ab47df6
Lucky <hilaryomondi08@gmail.com>
2026-08-22 11:00:14 +0300

    inetrgrated redis for caching purpose

--- app/database.py
+++ app/database.py
@@ -2,11 +2,18 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker,DeclarativeBase
 from dotenv import load_dotenv
+import redis
 
 load_dotenv()
+
+REDIS_HOST=os.getenv("REDIS_HOST","localhost")
+REDIS_PORT=os.getenv("REDIS_PORT",6379)
+
+redis_client=redis.Redis(host=REDIS_HOST, port=REDIS_PORT, decode_responses=True)
+
 DATABASE_URL=os.getenv("DATABASE_URL")
 
-engine=create_engine(DATABASE_URL,)
+engine=create_engine(DATABASE_URL)
 
 SessionLocal=sessionmaker(bind=engine)
 
