e5f82966768df02ffa1e1dc4043e1e12aaee5ebc
Lucky <hilaryomondi08@gmail.com>
2026-08-28 11:07:42 +0300

    implemented the function for generating acess tokens

--- app/security.py
+++ app/security.py
@@ -1,6 +1,21 @@
 from passlib.context import CryptContext
+from jose import jwt 
+from datetime import datetime, timedelta
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 pwd_context=CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+SECRET_KEY=os.getenv("SECRET_KEY")
+ALGORITHM=os.getenv("ALGORITHM")
+
+def create_access_token(data:dict, expires_delta:timedelta=timedelta(minutes=30)):
+    to_encode=data.copy()
+    expire=datetime.utcnow() + expires_delta
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
 def hash_password(password:str) -> str:
     return pwd_context.hash(password)
