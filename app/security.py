4ec8a31ba662772e87d9ef183d0e763be8b10159
Lucky <hilaryomondi08@gmail.com>
2026-08-31 13:56:00 +0300

    created a function that verifies a user token to allow acess to secured endpoint

--- app/security.py
+++ app/security.py
@@ -3,6 +3,12 @@ from jose import jwt
 from datetime import datetime, timedelta
 import os
 from dotenv import load_dotenv
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
+from fastapi import HTTPException, Depends
+from .models import Users
+from .database import get_db
+from sqlalchemy.orm import Session
 
 load_dotenv()
 
@@ -22,3 +28,22 @@ def hash_password(password:str) -> str:
 
 def verify_password(plain_password:str, hashed_password:str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
+
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id = payload.get("sub")
+        if user_id is None:
+            raise HTTPException(status_code=401, detail="Invalid token")
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid token")
+
+    user = db.query(Users).filter(Users.user_id == int(user_id)).first()
+    if user is None:
+        raise HTTPException(status_code=401, detail="User not found")
+
+    return user
