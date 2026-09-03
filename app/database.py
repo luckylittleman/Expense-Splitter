5d997e393c2acb6c6476185603e743a52b60b97e
Lucky <hilaryomondi08@gmail.com>
2026-09-03 13:56:38 +0300

    updated the redis url format to match that of the hosting service

--- app/database.py
+++ app/database.py
@@ -6,10 +6,16 @@ import redis
 
 load_dotenv()
 
-REDIS_HOST=os.getenv("REDIS_HOST","localhost")
-REDIS_PORT=os.getenv("REDIS_PORT",6379)
+REDIS_URL=os.getenv("REDIS_URL")
 
-redis_client=redis.Redis(host=REDIS_HOST, port=REDIS_PORT, decode_responses=True)
+if REDIS_URL:
+    redis_client = redis.Redis.from_url(REDIS_URL, decode_responses=True)
+
+else:
+
+ REDIS_HOST=os.getenv("REDIS_HOST","localhost")
+ REDIS_PORT=os.getenv("REDIS_PORT",6379)
+ redis_client=redis.Redis(host=REDIS_HOST, port=REDIS_PORT, decode_responses=True)
 
 DATABASE_URL=os.getenv("DATABASE_URL")
 
